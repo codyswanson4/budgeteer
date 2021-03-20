9fad7b30a9ffd6778935c8ade6582c2d8b2d7df6
Cody Swanson <cswanson@codyswanson.xyz>
2021-03-19 22:47:09 -0700

    Initial commit

---
+++ lib/Migration/Version000000Date202103190000.php
@@ -0,0 +1,67 @@
+<?php
+
+  namespace OCA\Budgeteer\Migration;
+
+  use Closure;
+  use OCP\DB\ISchemaWrapper;
+  use OCP\Migration\SimpleMigrationStep;
+  use OCP\Migration\IOutput;
+
+  classVersion000000Date202103190000 extends SimpleMigrationStep {
+    /**
+    * @param IOutput $output
+    * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+    * @param array $options
+    * @return null|ISchemaWrapper
+    */
+    public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+        /** @var ISchemaWrapper $schema */
+        $schema = $schemaClosure();
+
+        if (!$schema->hasTable('budget')) {
+            $table = $schema->createTable('budget');
+            $table->addColumn('id', 'integer', [
+                'autoincrement' => true,
+                'notnull' => true,
+            ]);
+            $table->addColumn('name', 'string', [
+                'notnull' => true,
+                'length' => 64
+            ]);
+            $table->addColumn('user_id', 'string', [
+                'notnull' => true,
+                'length' => 200,
+            ]);
+            $table->addColumn('content', 'text', [
+                'notnull' => true,
+                'default' => ''
+            ]);
+
+            $table->setPrimaryKey(['id']);
+            $table->addIndex(['user_id'], 'notestutorial_user_id_index');
+        }
+        if (!$schema->hasTable('budgeteer')) {
+            $table = $schema->createTable('budgeteer');
+            $table->addColumn('id', 'integer', [
+                'autoincrement' => true,
+                'notnull' => true,
+            ]);
+            $table->addColumn('title', 'string', [
+                'notnull' => true,
+                'length' => 200
+            ]);
+            $table->addColumn('user_id', 'string', [
+                'notnull' => true,
+                'length' => 200,
+            ]);
+            $table->addColumn('content', 'text', [
+                'notnull' => true,
+                'default' => ''
+            ]);
+
+            $table->setPrimaryKey(['id']);
+            $table->addIndex(['user_id'], 'notestutorial_user_id_index');
+        }
+        return $schema;
+    }
+}
